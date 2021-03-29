fcd7201457f2b17382dcd608f0a535147c2f11d6
Koki Kudo <massanmesu@icloud.com>
2021-03-29 09:50:44 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by kudo koki on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
