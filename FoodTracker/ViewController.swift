49e0d595d115c0a3a78ee4cc82d9f19fc7fdf9ce
Koki Kudo <massanmesu@icloud.com>
2021-03-29 22:38:32 +0900

    ライブラリから選択した写真を配置するUIImageViewを実装

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // プロパティ
     @IBOutlet weak var mealName: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     //4
     override func viewDidLoad() {
@@ -31,7 +32,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         mealName.text = textField.text
     }
-    
+    // 写真タップ時の処理
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // textFieldに入力中に写真をタップした場合、キーボードが閉じるようにする
+        nameTextField.resignFirstResponder()
+        // インスタンス生成
+        let imagePickerController = UIImagePickerController()
+        // 「ライブラリから選択」のみ実装
+        imagePickerController.sourceType = .photoLibrary
+        // imagePickerControllerデリゲート
+        imagePickerController.delegate = self
+        // UIImagePickerControllerに遷移させる
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    // imagePickerのキャンセルボタンを押した時の処理
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // 処理を却下する
+        dismiss(animated: true, completion: nil)
+    }
+    // imagePickerで写真を選択した時に実行。
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 選択した写真がUIImageに変換可能か確認
+        guard let selectedImage = info[.originalImage] as? UIImage
+        else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        // 適用する
+        photoImageView.image = selectedImage
+        // imagePickerを閉じる
+        dismiss(animated: true, completion: nil)
+    }
     //setDefaultLabelTextボタンを押した時に実行
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealName.text = "Default Text"
