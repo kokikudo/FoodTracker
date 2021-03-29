86e99b8616ff4387d6f9e825d4982e1d43d77ed7
Koki Kudo <massanmesu@icloud.com>
2021-03-29 17:23:01 +0900

    meal nameラベル、textField、Set Default Label Text ボタンを実装

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    // プロパティ
+    @IBOutlet weak var mealName: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     //4
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // textFieldの処理をデリゲートで対応
+        nameTextField.delegate = self
     }
-
-
+    
+    // UITextFieldDelegateの関数。各イベントに対する処理を記述
+    // texFieldShouldReturn: リターンキーを押した時の処理。基本的に戻り値はtrueで良い。
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 編集を完了する　→　ファーストレスポンダをやめる
+        textField.resignFirstResponder()
+        return true
+    }
+    // textFieldDidEndEditing: textFieldの編集が完了した時に実行
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealName.text = textField.text
+    }
+    
+    //setDefaultLabelTextボタンを押した時に実行
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealName.text = "Default Text"
+    }
+    
 }
 
