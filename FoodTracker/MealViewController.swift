f6b6fe26a17e3877966e7ec7e7c745aa161ebc7a
Koki Kudo <massanmesu@icloud.com>
2021-04-03 11:11:56 +0900

    新規料理ページ作成ボタンを作成 作成ボタンから料理編集ページに遷移するように実装

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import os.log
 
 class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // プロパティ
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    var meal: Meal? // 追加された料理データ
     
     //4
     override func viewDidLoad() {
@@ -19,17 +22,48 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         
         // textFieldの処理をデリゲートで対応
         nameTextField.delegate = self
+        
+        // 料理名を入力するまで「保存ボタン」無効
+        updateSaveButtomState()
     }
     
-    // UITextFieldDelegateの関数。各イベントに対する処理を記述
-    // texFieldShouldReturn: リターンキーを押した時の処理。基本的に戻り値はtrueで良い。
+    // ナビゲーション
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        // 保存ボタンが押された場合のみ宛先ViewControllerを設定
+        guard let button = sender as? UIBarButtonItem,
+              button === saveButton else { // 押されなかった時はコンソールに表示し戻る
+            os_log("The save button was not pressed, canselling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        // 保存した料理名、写真、評価点を格納
+        let name = nameTextField.text ?? "" // ?? = null合体演算子。値がある場合はオプショナル型で値を返し、nilの場合はデフォルト値（""）を返す
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        
+        // アンワインドセグエの後にMealTableViewContorollerに渡すMealインスタンスを生成
+        meal = Meal(name: name, photo: photo, rating: rating)
+    }
+    
+    // UITextFieldDelegateの関数。textField編集前後の処理
+    //　テキスト編集中は「保存」ボタンを無効にする
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        saveButton.isEnabled = false
+    }
+    // リターンキーを押した時の処理。基本的に戻り値はtrueで良い。
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // 編集を完了する　→　ファーストレスポンダをやめる
         textField.resignFirstResponder()
         return true
     }
-    // textFieldDidEndEditing: textFieldの編集が完了した時に実行
+    // textFieldの編集が完了した時に実行
     func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        // テキストが空かどうか確認し、テキスト内容をナビゲーションのタイトルに設定
+        updateSaveButtomState()
+        navigationItem.title = textField.text
     }
     // 写真タップ時の処理
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
@@ -62,5 +96,17 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         dismiss(animated: true, completion: nil)
     }
     
+    // キャンセルボタンで戻る
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // プライベートメソッド
+    private func updateSaveButtomState() {
+        
+        // textが空の場合は保存ボタンを無効にする
+        let text = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
+    }
 }
 
